33f5a5d2825652b534a4e0ec1d1d2f95d4fbb3b2
rajiv-shrivastav <rjv834@gmail.com>
2020-08-03 15:31:13 +0530

    more rows added

--- src/TableWithDrag.js
+++ src/TableWithDrag.js
@@ -9,18 +9,6 @@ import './assets/css/style.css'
 
 const DragHandle = sortableHandle(() => <span className="dragItem"></span>);
 
-const SortableItem = sortableElement(({value}) => (
-  <React.Fragment>
-    <tr>
-      <td> 
-        <DragHandle /> {value}
-        </td>
-        <td>         
-      </td>
-    </tr>
-  </React.Fragment>
-));
-
 const SortableContainer = sortableContainer(({children}) => {
   return <table className="table">
     <thead>
@@ -29,7 +17,7 @@ const SortableContainer = sortableContainer(({children}) => {
         Things To Do
       </th>
       <th>
-        Two
+        Owner
       </th>
       </tr>
     </thead>
@@ -39,17 +27,32 @@ const SortableContainer = sortableContainer(({children}) => {
     </table>;
 });
 
+const SortableItem = sortableElement(({title,owner,status,dueDate}) => (
+  <React.Fragment>
+    <tr>
+      <td> 
+        <DragHandle /> {title}
+        </td>
+        <td>
+          {owner}         
+        </td>
+        <td>
+          {status}         
+        </td>
+        <td>
+          {dueDate}         
+        </td>
+    </tr>
+  </React.Fragment>
+));
+
 export default class TableWithDrag extends Component {
   constructor(props){
     super(props)
     this.state = {
       todos: [
-        {title: 'Item 1'}, 
-        {title: 'Item 2'}, 
-        {title: 'Item 3'}, 
-        {title: 'Item 4'}, 
-        {title: 'Item 5'}, 
-        {title: 'Item 6'}
+        {title: 'Item 1',owner: 'anonyomous',status: 'Pending',dueDate: '12.08.2020'}, 
+        {title: 'Item 2',owner: 'anonyomous',status: 'Pending',dueDate: '12.08.2020'}
       ]
     }
   }
@@ -66,7 +69,14 @@ export default class TableWithDrag extends Component {
     return (
       <SortableContainer onSortEnd={this.onSortEnd} useDragHandle>
         {todos.map((todo, index) => (
-          <SortableItem key={`item-${todo.title}`} index={index} value={todo.title} />            
+          <SortableItem 
+            key={`item-${todo.title}`} 
+            index={index} 
+            title={todo.title} 
+            owner={todo.owner}
+            status={todo.status}
+            dueDate={todo.dueDate}
+            />            
         ))}
       </SortableContainer>
     );
